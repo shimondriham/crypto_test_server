b243641eef11d766c9c94dcd8aa7f242178c6bf4
shimon driham <95618903+shimondriham@users.noreply.github.com>
2024-12-25 13:16:41 +0200

    fix 1

--- routes/crypto.js
+++ routes/crypto.js
@@ -3,27 +3,29 @@ const express = require("express");
 const { auth } = require('../middlewares/auth');
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-    res.json({ msg: "Work from crypto.js" });
-});
+const apikey = "5D585955-6480-4964-B6B5-CD16D3FB883A"
 
-router.post("/",auth, async (req, res, next) => {
-    const newData = {
-         time_close_Data : [],
-        price_close_Data : [],
-    };
+
+router.get("/", auth, async (req, res, next) => {
     let limit = req.body.limit;
+    if(!limit){
+        return res.status(400).json("limit was not received");
+    }
+    const newData = {
+        time_close_data: [],
+        price_close_data: [],
+    };
     const today = new Date();
     today.setHours(0, 0, 0, 0);
     const formattedDate = today.toISOString().split('T')[0] + 'T00:00:00';
     try {
-        let response = await axios.get(`https://rest.coinapi.io/v1/ohlcv/BITSTAMP_SPOT_BTC_USD/history?apikey=5D585955-6480-4964-B6B5-CD16D3FB883A&period_id=1DAY&time_end=${formattedDate}&limit=${limit}`);
+        let response = await axios.get(`https://rest.coinapi.io/v1/ohlcv/BITSTAMP_SPOT_BTC_USD/history?apikey=${apikey}&period_id=1DAY&time_end=${formattedDate}&limit=${limit}`);
         for (let index = 0; index < response.data.length; index++) {
-            newData.time_close_Data[index]=response.data[index].time_close.split('T')[0];
-            newData.price_close_Data[index]=response.data[index].price_close;
+            newData.time_close_data[index] = response.data[index].time_close.split('T')[0];
+            newData.price_close_data[index] = response.data[index].price_close;
         }
-        newData.time_close_Data = [...newData.time_close_Data].reverse();
-        newData.price_close_Data = [...newData.price_close_Data].reverse();
+        newData.time_close_data = [...newData.time_close_data].reverse();
+        newData.price_close_data = [...newData.price_close_data].reverse();
         console.log(newData);
         res.status(200).json(newData);
     } catch (err) {
@@ -33,5 +35,3 @@ router.post("/",auth, async (req, res, next) => {
 });
 
 module.exports = router;
-
-//  https://rest.coinapi.io/v1/ohlcv/BITSTAMP_SPOT_BTC_USD/history?apikey=5D585955-6480-4964-B6B5-CD16D3FB883A&period_id=1DAY&time_end=2024-12-23T00:00:00&limit=30
